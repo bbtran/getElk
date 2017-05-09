4204e9664e8e0ed914ed0cc9e33afd927b96af5b
Benjamin Tran <benjamin.tran25@gmail.com>
2017-05-08 23:04:27 -0700

    [ DEV ] Initial commit. Setup dev env.

---
+++ webpack.prod.config.js
@@ -0,0 +1,60 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const PATHS = {
+  app: path.resolve(__dirname, './client'),
+  dist: path.resolve(__dirname, './dist'),
+  style: [path.resolve(__dirname, './styles')],
+};
+
+const VENDOR_LIBS = ['lodash', 'react', 'react-dom', 'redux', 'react-router', 'react-redux', 'react-router-redux'];
+
+module.exports = {
+  entry: {
+    app: [PATHS.app],
+    vendor: VENDOR_LIBS,
+  },
+  output: {
+    path: PATHS.dist,
+    filename: '[name].[hash].js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [{
+        test: /.*\.jsx?$/,
+        use: ['react-hot-loader/webpack', 'babel-loader'],
+        include: PATHS.app,
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.less$/,
+        include: PATHS.style,
+        use: ['style-loader', 'css-loader', 'less-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'GetElk',
+      template: PATHS.app + '/index.html',
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest'],
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      },
+    }),
+    new webpack.optimize.UglifyJsPlugin(),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  externals: {
+    'react/addons': true,
+    'react/lib/ExecutionEnvironment': true,
+    'react/lib/ReactContext': true,
+  },
+};
