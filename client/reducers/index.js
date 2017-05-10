8395397dcaa74d564ad7ceaedb947de7b0027175
Benjamin Tran <benjamin.tran25@gmail.com>
2017-05-10 13:17:10 -0700

    [ FEAT ] Store testimonials in redux store

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
-import { GET_PAGE_CONTENT } from '../actions/index';
+import { GET_PAGE_CONTENT, GET_TESTIMONIALS } from '../actions/index';
 
 function pageContentReducer(state = {}, action) {
   switch (action.type) {
@@ -11,8 +11,18 @@ function pageContentReducer(state = {}, action) {
   }
 }
 
+function testimonialsReducer(state = {}, action) {
+  switch (action.type) {
+    case GET_TESTIMONIALS:
+    return JSON.parse(action.payload) || state;
+    default:
+    return state;
+  }
+}
+
 const rootReducer = combineReducers({
   pageContent: pageContentReducer,
+  testimonialList: testimonialsReducer,
   router: routerReducer,
 });
 
