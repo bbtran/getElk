760480117ee73833e7c0acba0eabf46687339543
Benjamin Tran <benjamin.tran25@gmail.com>
2017-05-09 20:50:57 -0700

    [ FEAT ] Add main navbar

---
+++ client/containers/Navbar.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import NavbarListItem from '../components/NavbarListItem';
+import { NavbarBrand } from '../components/NavbarBrand';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="navigation-container">
+        <div className="main-nav">
+          <nav className="navbar">
+            <div className="navbar-header">
+              <NavbarBrand />
+            </div>
+            <div className="navbar-list">
+              <ul>
+                {this.props.pageContent.nav.items.reverse().map((item) => <NavbarListItem key={item.action} title={item.title} />)}
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps({ pageContent }) {
+  return { pageContent };
+}
+
+Navbar.propTypes = {
+  pageContent: PropTypes.object,
+};
+
+export default connect(mapStateToProps)(Navbar);
